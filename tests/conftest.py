a69b3552774535b495e575efe284ca674574d789
Tomas Kender <tomas.kender@oracle.com>
2023-09-09 14:46:35 +0200

    Test preprocessing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,19 +3,22 @@ from pathlib import Path
 import pytest
 import requests
 
+
 LOGGER = logging.getLogger()
-DATASET_URL = "https://github.com/treselle-systems/customer_churn_analysis/blob/master/WA_Fn-UseC_-Telco-Customer-Churn.csv"
-DATASET_FILENAME = "Telco-Customer-Churn.csv"
+
+
+def pytest_configure():
+    pytest.DATASET_URL = "https://raw.githubusercontent.com/treselle-systems/customer_churn_analysis/master/WA_Fn-UseC_-Telco-Customer-Churn.csv"
+    pytest.DATASET_PATH = Path(__file__).parent / "data" / "Telco-Customer-Churn.csv"
 
 
 @pytest.fixture(scope="session")
 def download_dataset():
-    url = DATASET_URL
+    url = pytest.DATASET_URL
     r = requests.get(url, allow_redirects=True)
-    file_path = Path(__file__).parent / "data" / DATASET_FILENAME
-    if not file_path.exists():
+    if not pytest.DATASET_PATH.exists():
         LOGGER.info("Downloading test dataset")
-        file_path.parent.mkdir(exist_ok=True)
-        file_path.write_bytes(r.content)
+        pytest.DATASET_PATH.parent.mkdir(exist_ok=True)
+        pytest.DATASET_PATH.write_bytes(r.content)
     else:
         LOGGER.info("Dataset found, skipping download")
--- tests/integration/test_preprocessing.py
+++ tests/integration/test_preprocessing.py
@@ -1,2 +1,20 @@
+import numpy as np
+from model.forest import preprocess
+import pytest
+
+
 def test_valid(download_dataset):
-    assert True
+    X, y = preprocess(pytest.DATASET_PATH)
+    assert len(X) == 10348
+    for col in ["customerID", "gender", "PhoneService", "Contract", "TotalCharges"]:
+        assert col not in X.columns
+
+    assert X["tenure"].min() >= 0. and X["tenure"].max() <= 1.
+    assert X["MonthlyCharges"].min() >= 0. and X["MonthlyCharges"].max() <= 1.
+
+    assert y.dtype == np.dtype("int")
+    assert y.min() == 0 and y.max() == 1
+
+    # Check for equal representation of churned and not churned clients
+    # value_counts for 0 and 1 should be the same, thus set of these 2 numbers is 1 number
+    assert len(set(y.value_counts())) == 1
